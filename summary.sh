e43b0f0cae3ab48c49636780269791ecd4ec8a86
ZyWaK <125191528+zwk-app@users.noreply.github.com>
2023-02-18 16:45:18 +0100

    Same bullet style as in .github/release.yml

--- summary.sh
+++ summary.sh
@@ -5,10 +5,10 @@ MD_OUT=SyncTimer.summary.md
 function MdHeader {
   echo -ne "## What's Changed\n\n" > "${MD_OUT}"
   # shellcheck disable=SC2129
-  echo -ne "### 🛠 Breaking Changes\n\n- LoremIpsum\n\n\n" >> "${MD_OUT}"
-  echo -ne "### 🎯 Features\n\n- LoremIpsum\n\n\n" >> "${MD_OUT}"
-  echo -ne "### 🩹 Fix:\n\n- LoremIpsum\n\n\n" >> "${MD_OUT}"
-  echo -ne "### 🧹 Other:\n\n- LoremIpsum\n\n\n" >> "${MD_OUT}"
+  echo -ne "### 🛠 Breaking Changes\n\n* LoremIpsum\n\n\n" >> "${MD_OUT}"
+  echo -ne "### 🎯 Features\n\n* LoremIpsum\n\n\n" >> "${MD_OUT}"
+  echo -ne "### 🩹 Fix:\n\n* LoremIpsum\n\n\n" >> "${MD_OUT}"
+  echo -ne "### 🧹 Other:\n\n* LoremIpsum\n\n\n" >> "${MD_OUT}"
 }
 
 function MdSumHead {
